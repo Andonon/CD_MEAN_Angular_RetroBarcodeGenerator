30691ae58d097627743a55ff0383589053aeb786
Troy Center <troycenter1@gmail.com>
2017-07-15 12:05:25 -0700

    finished assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = 'Retro Barcode Generator';
-  boxes: Array<number> = [grn(),grn(),grn(),grn(),grn(),grn(),grn(),grn(),grn()]
-}
+  boxes: Array<string> = []
 
-function grn(){
-  let value: number = (Math.floor(Math.random()*6))+1
-  return value;
-};
+  fillBoxes() {
+    for (let i = 0; i < 10; i++) {
+      const boxColor = (Math.floor(Math.random() * 10))+1;
+      if (boxColor === 1) {
+        this.boxes.push('OliveDrab');
+      } else if (boxColor === 2) {
+        this.boxes.push('PaleGreen');
+      } else if (boxColor === 3) {
+        this.boxes.push('SandyBrown');
+      } else if (boxColor === 4) {
+        this.boxes.push('SkyBlue');
+      } else if (boxColor === 5) {
+        this.boxes.push('Orange');
+      } else if (boxColor === 6) {
+        this.boxes.push('SlateBlue');
+      } else if (boxColor === 7) {
+        this.boxes.push('Black');
+      } else if (boxColor === 8) {
+        this.boxes.push('YellowGreen');
+      } else if (boxColor === 9) {
+        this.boxes.push('Thistle');
+      } else if (boxColor === 10) {
+        this.boxes.push('SpringGreen');
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.fillBoxes();
+  }
+
+}
